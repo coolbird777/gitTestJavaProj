60ac93ca5030d7b76206eccfc744892745824f65
wang <coolbird777@163.com>
2020-01-02 15:34:57 +0800

    添加了 TbGoods类

---
+++ gitTestJavaProj/src/entity/TbGoods.java
@@ -0,0 +1,26 @@
+package entity;
+
+public class TbGoods {
+	private Integer id;
+	private String goodsName;
+	private Double nowPrice;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public Double getNowPrice() {
+		return nowPrice;
+	}
+	public void setNowPrice(Double nowPrice) {
+		this.nowPrice = nowPrice;
+	}
+	
+}
