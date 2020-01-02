cc5de3ba18ba3c768e848f827393a36da74f9ed2
wang <coolbird777@163.com>
2020-01-02 14:47:47 +0800

    添加相关的getter和setter方法

--- gitTestJavaProj/src/entity/Person.java
+++ gitTestJavaProj/src/entity/Person.java
@@ -4,5 +4,23 @@ public class Person {
 	private Integer id;
 	private String username;
 	private String userpwd;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
 	
 }
